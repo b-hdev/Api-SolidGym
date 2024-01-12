fa6e6d7bb90fc7aaf9bbdc942e54a420fe5f7420
CyberPlank_2077 <bruno.bh969@gmail.com>
2024-01-12 11:54:20 -0300

    first commit

---
+++ src/use-cases/register.ts
@@ -0,0 +1,43 @@
+import { UsersRepository } from '@/repositories/users-repository';
+import { hash } from 'bcryptjs';
+import { UserAlreadyExistsError } from './errors/user-already-exists-error';
+import { User } from '@prisma/client';
+
+interface RegisterUseCaseSchema {
+  name: string;
+  email: string;
+  password: string;
+}
+
+interface RegisterUseCaseResponse {
+  user: User;
+}
+
+export class RegisterUseCase {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({
+    name,
+    email,
+    password,
+  }: RegisterUseCaseSchema): Promise<RegisterUseCaseResponse> {
+    const password_hash = await hash(password, 6);
+
+    const userWithSameEmail = await this.usersRepository.findByEmail(email);
+
+    if (userWithSameEmail) {
+      throw new UserAlreadyExistsError();
+    }
+
+    const user = await this.usersRepository.create({
+      name,
+      email,
+      password_hash,
+    });
+
+    return {
+      user,
+    };
+  }
+}
